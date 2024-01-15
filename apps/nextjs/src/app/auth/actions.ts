cd8189385619f925d0892214a323a7a7ce9d0f8e
aidenwong812 <wongaiden812@gmail.com>
2024-01-16 06:53:29 +1000

    add console log

--- apps/nextjs/src/app/auth/actions.ts
+++ apps/nextjs/src/app/auth/actions.ts
@@ -56,6 +56,8 @@ export const signInWithGoogle = async () => {
     provider: "google",
     options: { redirectTo: `${origin}/auth/callback` },
   });
+  console.log("origin ===>>>>", origin);
+  console.log("res ===>>>", res.data)
 
   if (res.data.url) redirect(res.data.url);
 
